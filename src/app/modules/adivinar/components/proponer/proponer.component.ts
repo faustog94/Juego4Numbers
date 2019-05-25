1cd9f9890cac203e7151fc7190006bc403781260
Fausto Carrizo <faustogcarrizo@gmail.com>
2019-05-24 23:49:03 -0300

    Primera version funcional parte B. Falta optimizar para que converja mas rapido a soluciones correctas

--- src/app/modules/adivinar/components/proponer/proponer.component.ts
+++ src/app/modules/adivinar/components/proponer/proponer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { sampleSize } from 'lodash';
-import { digitsToString } from '../../utils/util-numeros';
+import { digitsToString, getDigitos, contarCifrasCorrectas, contarAciertos, compararNumeros, hayRepetidos } from '../../utils/util-numeros';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
@@ -12,7 +12,7 @@ import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/fo
 
 export class ProponerComponent implements OnInit {
   lastNumber: Numero;
-  numerosPropuestos;
+  numerosPropuestos: Array<Numero> = [];
   lastFeedback;
   form = new FormGroup(
     {
@@ -32,9 +32,61 @@ export class ProponerComponent implements OnInit {
    }
 
   ngOnInit() {}
-  enviar() {}
+  enviar() {
+    const numeroAnterior = this.lastNumber.value;
+    // Guardar lo que ingresó la persona en desordenadas y ordenadas.
+    this.lastNumber.ordenadas = this.form.get('inputOrdenadas').value;
+    this.lastNumber.desordenadas = this.form.get('inputDesordenadas').value;
+    this.numerosPropuestos.push(this.lastNumber);
+    console.log(this.numerosPropuestos);
+    // Generar numero basado en lo ingresado.
+    if (this.lastNumber.ordenadas === 4) {
+      alert('Ganeeee madafaca');
+    }else{
+      this.form.reset();
+      this.lastNumber.value = this.generarNumero(numeroAnterior, this.lastNumber.ordenadas, this.lastNumber.desordenadas);
+    }
+    alert(`Nuevo numero: ${this.lastNumber.value}`);
+
+  }
+
+  /**
+   * Devuelve una puntuación, suma 2 si la cifra es correcta, y 1 para las desordenadas.
+   * @param numerito Objeto Numero a puntuar.
+   */
+  puntuarNumero(numerito: Numero) {
+    return 2 * numerito.ordenadas + numerito.desordenadas;
+  }
+  generarNumero(numeroBase: string = null, ordenadas: number = 0, desordenadas: number = 0) {
+    if (numeroBase) {
+      let numeroBaseInt = parseInt(numeroBase, 10);
+      let generado;
+      let retornable = false;
+      while (!retornable) {
+        numeroBaseInt++;
+        if (numeroBaseInt >= 10000){
+          numeroBaseInt = 123; // Reseteo!
+        }
+        generado = getDigitos(numeroBaseInt.toString());
+        // Para aquellos numeros que tienen un 0 y lo pierden en el parseInt.
+        generado = (generado.length === 4) ? generado : generado.unshift(0);
+        const comparacion = compararNumeros(generado, getDigitos(numeroBase));
+        const cntCorrectas = comparacion.ordenadas;
+        const cntDesordenadas = comparacion.desordenadas;
+        if (cntCorrectas === ordenadas && cntDesordenadas === desordenadas && !hayRepetidos(generado)) {
+          retornable = true;
+        }
+      }
+      console.log(digitsToString(generado));
+      return digitsToString(generado);
+    } else {
+      // Genera un numero totalmente aleatorio
+      return digitsToString(sampleSize([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4));
+    }
+  }
 }
-export function ValidarCantidades(control: AbstractControl) {
+
+function ValidarCantidades(control: AbstractControl) {
   const ordenadas = control.get('inputOrdenadas').value;
   const desordenadas = control.get('inputDesordenadas').value;
   if ((ordenadas + desordenadas) > 4) {
